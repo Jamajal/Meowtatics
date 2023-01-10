e4aee89b45e0cf9525dcbcf1ec5e70787389d5d4
Leandro Libério <leandroliberiomachado@hotmail.com>
2023-01-10 20:59:00 -0300

    Delete backend directory

--- backend/prisma/migrations/20221115021955_adding/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Cat" ADD COLUMN "description" TEXT;
--- backend/src/repositories/CatRepositories.ts
+++
@@ -1,7 +0,0 @@
-import { EntityRepository, Repository } from "typeorm"
-import { Cat } from "../entities/Cat"
-
-@EntityRepository(Cat)
-class CatRepositories extends Repository<Cat>{ }
-
-export { CatRepositories }
